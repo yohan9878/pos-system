d49167bbf4ac5e1c27165e879785da105ad1cfd6
Yohan Office <yohanoffice78@gmail.com>
2026-03-25 16:24:45 +0530

    Modified heading styles

--- app/components/CartTable.tsx
+++ app/components/CartTable.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useEffect, useEffectEvent, useState } from "react";
 import { CartItem } from "../types";
 
 interface Props {
@@ -8,37 +9,46 @@ interface Props {
 }
 
 export default function CartTable({ cart, onDelete }: Props) {
+   const [isClient, setIsClient] = useState(false)
+
+   const updateIsClient = useEffectEvent((isClient: boolean ) => {
+    setIsClient(isClient);
+   });
+
+  useEffect(() => {
+    updateIsClient(true)
+  }, [])
  
-  return (
-    <table className="w-full mt-5 border-collapse border border-gray-300">
+  return ( isClient &&
+    <table className=" w-full mt-5 border-collapse border border-gray-300">
       <thead>
         <tr>
-          <th className="border border-gray-300 p-2">Barcode</th>
-          <th className="border border-gray-300 p-2">Name</th>
+          <th className="border border-gray-300 p-2 w-30">Barcode</th>
+          <th className="border border-gray-300 p-2 w-100">Name</th>
           <th className="border border-gray-300 p-2">Qty</th>
           <th className="border border-gray-300 p-2">Price</th>
-          <th className="border border-gray-300 p-2">Total</th>
+          <th className="border border-gray-300 p-2 w-20">Total</th>
           <th className="border border-gray-300 p-2">Action</th>
         </tr>
       </thead>
       <tbody>
         {cart.map((item, idx) => (
           <tr key={idx}>
-            <td className="border border-gray-300 p-2">{item.barcode}</td>
+            <td className="border border-gray-300 p-2 text-center">{item.barcode}</td>
             <td className="border border-gray-300 p-2">{item.name}</td>
             <td className="border border-gray-300 p-2 text-center">
               {item.qty}
             </td>
             <td className="border border-gray-300 p-2 text-right">
-              {item.price}
+              {item.price.toFixed(2)}
             </td>
             <td className="border border-gray-300 p-2 text-right">
-              {item.price * item.qty}
+              {(item.price * item.qty).toFixed(2)}
             </td>
-            <td className="border p-2 text-center">
+            <td className="border border-gray-300 p-2 text-center">
               <button
                 onClick={() => onDelete(item.barcode)}
-                className="bg-red-500 text-white px-3 py-1 rounded hover:bg-red-600"
+                className=" text-white px-3 py-1 rounded hover:bg-red-600"
               >
                 ❌
               </button>
@@ -47,5 +57,5 @@ export default function CartTable({ cart, onDelete }: Props) {
         ))}
       </tbody>
     </table>
-  );
+ );
 }
--- app/components/TotalDisplay.tsx
+++ app/components/TotalDisplay.tsx
@@ -1,4 +1,6 @@
 "use client";
+
+import { useEffect, useEffectEvent, useState } from "react";
 import { CartItem } from "../types";
 
 interface Props {
@@ -6,6 +8,20 @@ interface Props {
 }
 
 export default function TotalDisplay({ cart }: Props) {
-  const total = cart.reduce((sum, item) => sum + item.price * item.qty, 0);
-  return <h2 className="text-2xl font-bold mt-4"> Sub Total: Rs. {total}</h2>;
+  const [isClient, setIsClient] = useState(false);
+
+ const updateIsClient = useEffectEvent((isClient: boolean ) => {
+     setIsClient(isClient);
+    });
+ 
+   useEffect(() => {
+     updateIsClient(true)
+   }, [])
+
+  const total = cart.reduce((sum, item) => sum + item.price * item.qty, 0).toFixed(2);
+  return (isClient &&
+    <div>
+      <h2 className="text-2xl font-bold mt-4"> Sub Total: Rs. {total}</h2>
+    </div>
+  );
 }
