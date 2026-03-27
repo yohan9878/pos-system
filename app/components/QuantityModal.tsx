93fe84a8c6e1a37904a4964372ac0d78e5181d71
Yohan Office <yohanoffice78@gmail.com>
2026-03-27 09:38:14 +0530

    hydration fix: only render on client side

--- app/components/QuantityModal.tsx
+++ app/components/QuantityModal.tsx
@@ -18,6 +18,16 @@ export default function QuantityModal({
   productName,
 }: Props) {
   const [qty, setQty] = useState<number>(initialQty);
+  const [isClient, setIsClient] = useState(false);
+
+  // hydration fix: only render on client side
+  const updateIsClient = useEffectEvent((isClient: boolean) => {
+    setIsClient(isClient);
+  });
+
+  useEffect(() => {
+    updateIsClient(true);
+  }, []);
 
   const updateQty = useEffectEvent((initialQty: number) => {
     setQty(initialQty);
@@ -31,56 +41,58 @@ export default function QuantityModal({
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-40 flex items-center justify-center">
-      <div className="bg-white p-6 rounded-lg shadow-lg w-80">
-        <h2 className="text-black text-xl font-bold mb-4">Update Quantity</h2>
+    isClient && (
+      <div className="fixed inset-0 bg-black bg-opacity-40 items-center justify-center ">
+        <div className="bg-white p-6 rounded-lg shadow-lg w-80">
+          <h2 className="text-black text-xl font-bold mb-4">Update Quantity</h2>
 
-        <p className="mb-2 text-gray-900">{productName}</p>
+          <p className="mb-2 text-gray-900">{productName}</p>
 
-        <input
-          type="number"
-          value={qty}
-          // onChange={(e) => setQty(parseInt(e.target.value))}
-          onChange={(e) => {
-            const value = parseInt(e.target.value);
-            setQty(isNaN(value) ? 0 : value);
-          }}
-          onKeyDown={(e) => {
-            if (e.key === "Enter") {
-              if (!qty || qty <= 0) return;
-              onConfirm(qty);
-              onClose();
-            }
-
-            // Optional: ESC to close
-            if (e.key === "Escape") {
-              onClose();
-            }
-          }}
-          autoFocus
-          className="w-full p-2 border rounded mb-4 text-gray-900"
-        />
-
-        <div className="flex justify-end gap-2">
-          <button
-            onClick={onClose}
-            className="px-4 py-2 bg-red-500 hover:bg-red-600 rounded"
-          >
-            Cancel
-          </button>
-
-          <button
-            onClick={() => {
-              if (!qty || qty <= 0) return;
-              onConfirm(qty);
-              onClose();
+          <input
+            type="number"
+            value={qty}
+            // onChange={(e) => setQty(parseInt(e.target.value))}
+            onChange={(e) => {
+              const value = parseInt(e.target.value);
+              setQty(isNaN(value) ? 0 : value);
             }}
-            className="px-4 py-2 bg-blue-500 text-white rounded"
-          >
-            Update
-          </button>
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                if (!qty || qty <= 0) return;
+                onConfirm(qty);
+                onClose();
+              }
+
+              // Optional: ESC to close
+              if (e.key === "Escape") {
+                onClose();
+              }
+            }}
+            autoFocus
+            className="w-full p-2 border rounded mb-4 text-gray-900"
+          />
+
+          <div className="flex justify-end gap-2">
+            <button
+              onClick={onClose}
+              className="px-4 py-2 bg-red-500 hover:bg-red-600 rounded"
+            >
+              Cancel
+            </button>
+
+            <button
+              onClick={() => {
+                if (!qty || qty <= 0) return;
+                onConfirm(qty);
+                onClose();
+              }}
+              className="px-4 py-2 bg-blue-500 text-white rounded"
+            >
+              Update
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    )
   );
 }
