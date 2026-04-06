345a756e84a6752184d5a3bf2085021433dcb366
Yohan Office <yohanoffice78@gmail.com>
2026-04-06 15:22:33 +0530

    Modified landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,53 @@
+"use client";
+
 import Link from "next/link";
+import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [time, setTime] = useState("");
+
+  useEffect(() => {
+    const updateTime = () => {
+      const now = new Date();
+      setTime(now.toLocaleTimeString("en-US", { timeStyle: "medium" }));
+    };
+    updateTime();
+    const interval = setInterval(updateTime, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div className="h-screen bg-white flex flex-col items-center justify-center gap-4">
-      <h1 className="text-4xl text-red-950 font-bold mb-4">Weehena Farm Shop</h1>
+    <div className="h-screen bg-white flex flex-col items-center justify-center">
+      <div className="flex flex-col mx-aut items-center gap-2">
+        <Image
+          src="/weehenaLogo.png"
+          alt="Weehena Farm Shop Logo"
+          width={150}
+          height={150}
+          className="size-18"
+        />
+        <h1 className="text-2xl text-black font-bold">Weehena Farm Shop</h1>
+        <p className="text-md text-gray-800 font-sans font-normal">
+          {new Date().toLocaleDateString("en-US", { dateStyle: "full" })}
+        </p>
+        <p className="text-md text-gray-800 font-sans font-normal">{time}</p>
+      </div>
 
-      <Link
-        href="/outlet/katunayake/scan"
-        className="bg-red-900 hover:bg-red-800 font-semibold text-white px-6 py-3 rounded"
-      >
-        Go to POS (Katunayake)
-      </Link>
-
-      <Link
-        href="/office/products"
-        className="bg-red-900 hover:bg-red-800 font-semibold text-white px-6 py-3 rounded"
-      >
-        Office Panel
-      </Link>
+      <div className="flex flex-col gap-4 mt-6">
+        <Link
+          href="/outlet/katunayake/scan"
+          className="w-56 text-center shadow drop-shadow-xl shadow-gray-500 bg-red-700 text-sm hover:bg-red-600 font-medium text-white px-6 py-3 rounded-xl"
+        >
+          Go to POS (Katunayake)
+        </Link>
+        <Link
+          href="/office/products"
+          className="w-56 text-center shadow drop-shadow-lg shadow-gray-600 bg-red-700 text-sm hover:bg-red-600 font-medium text-white px-6 py-3 rounded-xl"
+        >
+          Office Dashboard
+        </Link>
+      </div>
     </div>
   );
 }
