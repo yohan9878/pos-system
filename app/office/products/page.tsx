6906287589305578d25fa511393bc7755ed19a3a
Yohan Office <yohanoffice78@gmail.com>
2026-04-07 14:49:23 +0530

    Sorted table data by barcode and modified styles

--- app/office/products/page.tsx
+++ app/office/products/page.tsx
@@ -1,7 +1,12 @@
 "use client";
 
 import Button from "@/app/components/Button";
-import { addProduct, deleteProduct, getProducts, ProductItems } from "@/app/services/productService";
+import {
+  addProduct,
+  deleteProduct,
+  getProducts,
+  ProductItems,
+} from "@/app/services/productService";
 import { useEffect, useEffectEvent, useState } from "react";
 
 export default function ProductPage() {
@@ -16,7 +21,10 @@ export default function ProductPage() {
   const loadProducts = async () => {
     try {
       const data = await getProducts();
-      setProducts(data);
+      const sortedData = data.sort((a: ProductItems, b: ProductItems) =>
+        a.barcode.toString().localeCompare(b.barcode.toString()),
+      );
+      setProducts(sortedData);
     } catch (err) {
       console.error("Failed to fetch products:", err);
     }
@@ -52,48 +60,50 @@ export default function ProductPage() {
   };
 
   // Delete product
-    const handleDelete = async (id: number) => {
-      const confirmDelete = confirm("Are you sure to delete this product?");
-      if (!confirmDelete) return;
-  
-      try {
-        await deleteProduct(id);
-        await loadProducts();
-      } catch (err) {
-        console.error("Failed to delete product:", err);
-        alert("Failed to delete product");
-      }
-    };
+  const handleDelete = async (id: number) => {
+    const confirmDelete = confirm("Are you sure to delete this product?");
+    if (!confirmDelete) return;
+
+    try {
+      await deleteProduct(id);
+      await loadProducts();
+    } catch (err) {
+      console.error("Failed to delete product:", err);
+      alert("Failed to delete product");
+    }
+  };
 
   const filteredProducts = products.filter((p) => p.barcode.includes(search));
 
   return (
     <div className="text-white rounded">
-      <h1 className="text-2xl text-red-950 font-bold">Product Management</h1>
+      <h1 className="text-2xl text-red-950 font-bold mb-4">
+        Product Management
+      </h1>
       <input
         placeholder="Search by barcode..."
         value={search}
         onChange={(e) => setSearch(e.target.value)}
-        className="p-2 border mt-4 mb-4 w-50 border-gray-300 text-gray-700 rounded focus:outline-none focus:ring-2 focus:ring-red-800"
+        className="p-2 border mb-4 w-full bg-blue-50 border-gray-300 text-gray-700 rounded focus:outline-none focus:ring-2 focus:ring-red-800"
       />
       <div className="rounded mb-6 flex gap-2 flex-wrap ">
         <input
           placeholder="Barcode"
           value={barcode}
           onChange={(e) => setBarcode(e.target.value)}
-          className="w-50 p-2 text-md text-gray-700 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-red-800"
+          className="w-50 bg-green-50 p-2 text-md text-gray-700 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-red-800"
         />
         <input
           placeholder="Product Name"
           value={name}
           onChange={(e) => setName(e.target.value)}
-          className="w-96 p-2 text-gray-700 text-md border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-red-800"
+          className="w-96 bg-green-50 p-2 text-gray-700 text-md border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-red-800"
         />
         <input
           placeholder="Price"
           value={price}
           onChange={(e) => setPrice(e.target.value)}
-          className="w-50 p-2 text-gray-700 text-md border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-red-800"
+          className="w-50 bg-green-50 p-2 text-gray-700 text-md border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-red-800"
         />
         <Button
           onClick={handleAddProduct}
@@ -103,36 +113,36 @@ export default function ProductPage() {
         </Button>
       </div>
 
-      <table className="w-fit mt-5 border-collapse border border-gray-800">
+      <table className="w-full border border-gray-200">
         <thead>
           <tr>
-            <th className="border text-red-900 border-gray-800 p-2 w-50">
+            <th className="text-red-900 text-left border-gray-800 p-2 w-50">
               Barcode
             </th>
-            <th className="border text-red-900 border-gray-800 p-2 w-100">
+            <th className="text-red-900 text-left border-gray-800 p-2 w-100">
               Name
             </th>
-            <th className="border text-red-900 border-gray-800 p-2 w-50">
+            <th className="text-red-900 text-right border-gray-800 p-2 w-50">
               Price (Rs.)
             </th>
-            <th className="border text-red-900 border-gray-800 p-2 w-38">
+            <th className="text-red-900 border-gray-800 p-2 w-38">
               Action
             </th>
           </tr>
         </thead>
         <tbody>
           {filteredProducts.map((p) => (
-            <tr key={p.id}>
-              <td className="border text-gray-950 border-gray-500 p-2">
+            <tr key={p.id} className="odd:bg-gray-200 even:bg-white">
+              <td className=" text-gray-950 border-gray-500 font-medium p-2">
                 {p.barcode}
               </td>
-              <td className="border text-gray-950 border-gray-500 p-2">
+              <td className="text-gray-950 border-gray-500 font-medium p-2">
                 {p.name}
               </td>
-              <td className="border text-gray-950 border-gray-500 p-2 text-right">
+              <td className="text-gray-950 border-gray-500 font-medium p-2 text-right">
                 {p.price.toFixed(2)}
               </td>
-              <td className="border text-gray-950 border-gray-500 p-2 text-center w-fit">
+              <td className="text-gray-950 border-gray-500 p-2 text-center w-fit">
                 <Button
                   onClick={() => handleDelete(p.id)}
                   className="bg-red-900 hover:bg-red-700 text-white ml-4"
