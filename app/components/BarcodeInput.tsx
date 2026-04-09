c1bba27c18d3ce98809615550535331169d9b72a
Yohan Office <yohanoffice78@gmail.com>
2026-04-09 13:56:54 +0530

    Fixed Styles

--- app/components/BarcodeInput.tsx
+++ app/components/BarcodeInput.tsx
@@ -26,7 +26,7 @@ export default function BarcodeInput({
       onChange={(e) => setBarcode(e.target.value)}
       onKeyDown={(e) => e.key === "Enter" && handleAdd()}
       placeholder="Scan or enter barcode"
-      className="w-72 p-3 text-gray-700 text-lg border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-red-800"
+      className="w-72 p-3 text-gray-700 text-lg border bg-red-100 border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-red-800"
     />
   );
 }
--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -11,7 +11,7 @@ export default function Button({ onClick, children, className }: Props) {
   return (
     <button
       onClick={onClick}
-      className={`px-5 py-2 mr-3 bg-blue-700 text-white rounded hover:bg-blue-600 transition ${className}`}
+      className={`px-5 py-2 mr-3 text-white rounded transition ${className}`}
     >
       {children}
     </button>
