8ff17f2715983afa5082e3bc8e02cabb5b9485cf
Yohan Office <yohanoffice78@gmail.com>
2026-04-16 15:53:40 +0530

    Added new product attributes

--- app/office/products/page.tsx
+++ app/office/products/page.tsx
@@ -13,7 +13,12 @@ export default function ProductPage() {
   const [products, setProducts] = useState<ProductItems[]>([]);
   const [name, setName] = useState("");
   const [barcode, setBarcode] = useState("");
-  const [price, setPrice] = useState("");
+  const [bulkPrice, setBulkPrice] = useState("");
+  const [retailPrice, setRetailPrice] = useState("");
+  const [packPrice, setPackPrice] = useState("");
+  const [packWeight, setPackWeight] = useState("");
+  const [pricePerKg, setPricePerKg] = useState("");
+  const [weighted, setWeighted] = useState("false");
 
   const [search, setSearch] = useState("");
 
@@ -39,12 +44,26 @@ export default function ProductPage() {
   }, []);
 
   const handleAddProduct = async () => {
-    if (!name || !barcode || !price) return;
+    if (
+      !name ||
+      !barcode ||
+      !bulkPrice ||
+      !retailPrice ||
+      !packPrice ||
+      !packWeight ||
+      !pricePerKg
+    )
+      return;
 
     const newProduct = {
       name,
       barcode,
-      price: parseFloat(price),
+      bulkPrice: parseFloat(bulkPrice),
+      retailPrice: parseFloat(retailPrice),
+      packPrice: parseFloat(packPrice),
+      packWeight: parseFloat(packWeight),
+      pricePerKg: parseFloat(pricePerKg),
+      weighted: weighted === "true",
     };
 
     try {
@@ -52,7 +71,12 @@ export default function ProductPage() {
       await loadProducts(); // refresh list
       setName("");
       setBarcode("");
-      setPrice("");
+      setBulkPrice("");
+      setRetailPrice("");
+      setPackPrice("");
+      setPackWeight("");
+      setPricePerKg("");
+      setWeighted("false");
     } catch (err) {
       console.error("Failed to add product:", err);
       alert("Failed to add product");
@@ -73,9 +97,10 @@ export default function ProductPage() {
     }
   };
 
-  const filteredProducts = products.filter((item) =>
+  const filteredProducts = products.filter(
+    (item) =>
       item.barcode?.toString().includes(search) ||
-      item.name?.toLowerCase().includes(search.toLowerCase())
+      item.name?.toLowerCase().includes(search.toLowerCase()),
   );
 
   return (
@@ -86,29 +111,70 @@ export default function ProductPage() {
 
       <div className="rounded mb-4 flex gap-2 flex-wrap text-xs">
         <input
+          id="search"
           placeholder="Search by barcode..."
           value={search}
           onChange={(e) => setSearch(e.target.value)}
           className="p-2 mb-2 border w-full bg-blue-50 border-gray-300 text-gray-700 rounded focus:outline-none focus:ring-2 focus:ring-red-800"
         />
         <input
+          id="barcode"
           placeholder="Barcode"
           value={barcode}
           onChange={(e) => setBarcode(e.target.value)}
           className="w-50 bg-green-50 p-2 text-md text-gray-700 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-red-800"
         />
         <input
+          id="productName"
           placeholder="Product Name"
           value={name}
           onChange={(e) => setName(e.target.value)}
           className="w-96 bg-green-50 p-2 text-gray-700 text-md border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-red-800"
         />
         <input
-          placeholder="Price"
-          value={price}
-          onChange={(e) => setPrice(e.target.value)}
-          className="w-50 bg-green-50 p-2 text-gray-700 text-md border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-red-800"
+          id="bulkPrice"
+          placeholder="Bulk Price"
+          value={bulkPrice}
+          onChange={(e) => setBulkPrice(e.target.value)}
+          className="w-40 bg-green-50 p-2 text-gray-700 text-md border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-red-800"
         />
+        <input
+          id="retailPrice"
+          placeholder="Retail Price"
+          value={retailPrice}
+          onChange={(e) => setRetailPrice(e.target.value)}
+          className="w-40 bg-green-50 p-2 text-gray-700 text-md border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-red-800"
+        />
+        <input
+          id="packPrice"
+          placeholder="Pack Price"
+          value={packPrice}
+          onChange={(e) => setPackPrice(e.target.value)}
+          className="w-40 bg-green-50 p-2 text-gray-700 text-md border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-red-800"
+        />
+        {/* <input
+          id="packWeight"
+          placeholder="Pack Weight"
+          value={packWeight}
+          onChange={(e) => setPackWeight(e.target.value)}
+          className="w-40 bg-green-50 p-2 text-gray-700 text-md border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-red-800"
+        /> */}
+        <input
+          id="pricePerKg"
+          placeholder="Price per Kg"
+          value={pricePerKg}
+          onChange={(e) => setPricePerKg(e.target.value)}
+          className="w-40 bg-green-50 p-2 text-gray-700 text-md border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-red-800"
+        />
+        <select
+          id="weighted"
+          value={weighted}
+          onChange={(e) => setWeighted(e.target.value)}
+          className="w-40 bg-green-50 p-2 text-gray-700 text-md border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-red-800"
+        >
+          <option value="false">Not Weighted</option>
+          <option value="true">Weighted</option>
+        </select>
         <Button
           onClick={handleAddProduct}
           className=" bg-green-900 hover:bg-green-700"
@@ -126,8 +192,20 @@ export default function ProductPage() {
             <th className="text-red-900 text-left border-gray-800 p-2 w-100">
               Name
             </th>
-            <th className="text-red-900 text-right border-gray-800 p-2 w-50">
-              Price (Rs.)
+            <th className="text-red-900 text-right border-gray-800 p-2 w-30">
+              Bulk Price
+            </th>
+            <th className="text-red-900 text-right border-gray-800 p-2 w-30">
+              Retail Price
+            </th>
+            <th className="text-red-900 text-right border-gray-800 p-2 w-30">
+              Pack Price
+            </th>
+            {/* <th className="text-red-900 text-right border-gray-800 p-2 w-30">
+              Pack Weight
+            </th> */}
+            <th className="text-red-900 text-right border-gray-800 p-2 w-30">
+              Price per Kg
             </th>
             <th className="text-red-900 border-gray-800 p-2 w-38">Action</th>
           </tr>
@@ -141,8 +219,20 @@ export default function ProductPage() {
               <td className="text-gray-950 border-gray-500 font-medium p-2">
                 {p.name}
               </td>
-              <td className="text-gray-950 border-gray-500 font-medium p-2 text-right">
-                {p.price.toFixed(2)}
+              <td className="text-gray-950 border-gray-500 p-2 text-right">
+                {p.bulkPrice.toFixed(2)}
+              </td>
+              <td className="text-gray-950 border-gray-500 p-2 text-right">
+                {p.retailPrice.toFixed(2)}
+              </td>
+              <td className="text-gray-950 border-gray-500 p-2 text-right">
+                {p.packPrice ? p.packPrice.toFixed(2) : "N/A"}
+              </td>
+              {/* <td className="text-gray-950 border-gray-500 p-2 text-right">
+                {p.packWeight ? p.packWeight.toFixed(2) : "N/A"}
+              </td> */}
+              <td className="text-gray-950 border-gray-500 p-2 text-right">
+                {p.pricePerKg ? p.pricePerKg.toFixed(2) : "N/A"}
               </td>
               <td className="text-gray-950 border-gray-500 p-2 text-center w-fit">
                 <Button
