c5a194332e0c832652a593485983847c74f91870
yohan9878 <yohanasitha98@gmail.com>
2026-04-25 12:48:01 +0530

    Updated sale service

--- app/services/saleService.ts
+++ app/services/saleService.ts
@@ -1,6 +1,6 @@
 const BASE_URL = "http://localhost:8080/api/sales";
 
-interface SaleData {
+export interface SaleData {
   date: string;
   invoiceNo: string;
   outletId: string;
@@ -27,20 +27,4 @@ export const processSale = async (saleData: SaleData) => {
   }
 
   return response.text();
-};
-
-export const getSales = async (date: string, outletId?: string) => {
-  let url = `${BASE_URL}/daily?date=${date}&outletId=${outletId}`;
-
-  if (outletId && outletId !== "") {
-    url += `&outletId=${outletId}`;
-  }
-
-  const res = await fetch(url);
-
-  if (!res.ok) {
-    throw new Error("Failed to fetch sales");
-  }
-
-  return res.json();
 };
